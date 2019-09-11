e88b3c0d53f30668c8155f24aff9a85a5bf4bd3d
Silvestre Luna <slvtrluna@gmail.com>
2019-09-10 21:03:06 -0500

    Add project files.

---
+++ CollectionsLinq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CollectionsLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
