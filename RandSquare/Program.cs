dfb00839807fb3b5399bd57184703d6be5b7aaa5
Silvestre Luna <slvtrluna@gmail.com>
2019-09-13 19:25:22 -0500

    Created 3 number list. List of random numbers, list of squared rand number, list of original list, only even numbers

---
+++ RandSquare/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RandSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            var myNums = new List<int>();
+            var mySquaredNums = new List<int>();
+            var myEvenSquaredNum = new List<int>();
+
+            var counter = 1;
+            while (counter <= 20)
+            {
+                var randomNumber = rnd.Next(1, 50);
+
+                var tempSquaredNum = (int)Math.Pow(randomNumber, 2);
+
+                myNums.Add(randomNumber);
+                mySquaredNums.Add(tempSquaredNum);
+                counter++;
+            }
+            
+            foreach(var currentSquaredNum in mySquaredNums)
+            {
+                Console.WriteLine($"Final squared number is: {currentSquaredNum}");
+                if (currentSquaredNum % 2 == 0)
+                {
+                    myEvenSquaredNum.Add(currentSquaredNum);
+                }
+            }
+
+            foreach(var currentEvenNum in myEvenSquaredNum)
+            {
+                Console.WriteLine($"***This are my even numbers: {currentEvenNum}");
+            }
+        }
+    }
+}
